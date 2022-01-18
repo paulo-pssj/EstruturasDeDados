b3ca845afe931b94dddf9450ee646d12886fefba
paulo-pssj <paulinhops99@gmail.com>
2022-01-18 13:25:54 -0300

    Estudo de Filas em python

--- ListaEncadeada.py
+++ ListaEncadeada.py
@@ -76,3 +76,14 @@ class ListaEncadeada:
             corrente = corrente.proximo
 
         return corrente
+    
+
+lista = ListaEncadeada()
+for i in range(5):
+    lista.insere_no_inicio(i)
+print("Lista:", lista)
+
+print("Removendo elementos:")
+for i in range(5):
+    lista.remove(i)
+    print("Removendo o elemento {0}: {1}".format(i, lista))
